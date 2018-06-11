ff5b48ce1252b7b6a6805e08cf879d6eb94e74d8
Joe Donnelly <jdonnelly@hugeinc.com>
2018-06-11 12:03:04 -0400

    Added Firebase to iOS build via a podfile

---
+++ ios/Pods/Target Support Files/Pods-TechLunchApp/Pods-TechLunchApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TechLunchApp : NSObject
+@end
+@implementation PodsDummy_Pods_TechLunchApp
+@end
--- ios/TechLunchApp/AppDelegate.m
+++ ios/TechLunchApp/AppDelegate.m
@@ -9,6 +9,7 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import Firebase;
 
 @implementation AppDelegate
 
@@ -18,6 +19,7 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
+  [FIRApp configure];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"TechLunchApp"
                                                initialProperties:nil
