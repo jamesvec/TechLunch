6bc95824b5e8b4abe1bfb84387e2be1dec815109
Gary Acers <gacers@hugeinc.com>
2018-06-12 10:13:06 -0400

    basic map

--- App/Containers/Styles/GraphScreenStyle.js
+++ App/Containers/Styles/GraphScreenStyle.js
@@ -1,6 +1,12 @@
 import { StyleSheet } from 'react-native'
-import { ApplicationStyles } from '../../Themes/'
+import { ApplicationStyles, Metrics } from "../../Themes/";
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
-})
+  ...ApplicationStyles.screen,
+  map: {
+    flex: 1,
+    height: Metrics.screenHeight,
+    padding: 0,
+    width: Metrics.screenWeight
+  }
+});
